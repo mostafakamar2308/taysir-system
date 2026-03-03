f1709a28d771b2708d231fa40f6940b41f1ea2a7
Mostafa Kamar <moustafa.31011197@med.tanta.edu.eg>
2026-03-03 14:23:43 +0200

    feat: added prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+"use server";
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+const globalForPrisma = global as unknown as {
+  db: PrismaClient;
+};
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const db =
+  globalForPrisma.db ||
+  new PrismaClient({
+    adapter,
+  });
+if (process.env.NODE_ENV !== "production") globalForPrisma.db = db;
+export default db;
